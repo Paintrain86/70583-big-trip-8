c30d09543e18274a334749aff5a3c81bbd91b371
Paintrain86 <webmaster.karpov@yandex.ru>
2019-02-26 18:11:02 +0300

    project configuring, two modules

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import getFilter from './get-filter.js';
+import getPoint from './get-point.js';
+
+(function initPage() {
+  const pointsCount = {
+    min: 0,
+    max: 10,
+    default: 7
+  };
+
+  const getRandomIntegerFromRange = (min = 0, max = 5) => Math.floor(min + Math.random() * (max - min));
+
+  const initFilters = (onChange) => {
+    const filterItems = [
+      {
+        name: `everything`,
+        isChecked: true
+      },
+      {
+        name: `future`
+      },
+      {
+        name: `past`
+      }
+    ];
+    const filterBlock = document.querySelector(`.trip-filter`);
+
+    const onFilterChange = (e) => {
+      e.preventDefault();
+
+      onChange();
+    };
+
+    const renderFilters = (filters) => {
+      const filtersHtml = filters.map((item) => {
+        return getFilter(item.name, item.isChecked);
+      }).join(``);
+
+      filterBlock.innerHTML = ``;
+      filterBlock.insertAdjacentHTML(`afterBegin`, filtersHtml);
+      filterBlock.addEventListener(`change`, onFilterChange);
+    };
+
+    renderFilters(filterItems);
+  };
+
+  const renderPoints = () => {
+    const pointsBlock = document.querySelector(`.trip-day__items`);
+    const count = getRandomIntegerFromRange(pointsCount.min, pointsCount.max);
+    let pointsHtml = ``;
+
+    for (let i = 1; i <= count; i++) {
+      pointsHtml += getPoint();
+    }
+
+    pointsBlock.innerHTML = ``;
+    pointsBlock.insertAdjacentHTML(`afterBegin`, pointsHtml);
+  };
+
+  initFilters(renderPoints);
+  renderPoints();
+})();
