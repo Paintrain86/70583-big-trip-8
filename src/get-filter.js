373974270398164d0b821bd38760d1b66457de13
Eugene Karpov <webmaster.karpov@yandex.ru>
2019-04-14 13:26:40 +0300

    removed get filter file

--- src/get-filter.js
+++
@@ -1,6 +0,0 @@
-const getFilter = (name, isChecked = false) => `
-  <input type="radio" id="filter-${name}" name="filter" value="${name}" ${isChecked ? `checked` : ``}>
-  <label class="trip-filter__item" for="filter-${name}">${name}</label>
-`;
-
-export default getFilter;
